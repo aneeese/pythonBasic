3495bfed49caeed5c59588c0137f8901a681218c
aneese421@gmail.com <aneese421@gmail.com>
2022-02-21 21:51:18 +0500

    delete

--- Youtube.py
+++
@@ -1,6 +0,0 @@
-import pytube  
-from pytube import YouTube  
-video_url = 'https://www.youtube.com/watch?v=HrFlFMK1mKI'   
-youtube = pytube.YouTube(video_url)  
-video = youtube.streams.first()  
-video.download('C:/Users/Desktop/')
